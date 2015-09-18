fb0e51ff34a1222aaab8cca5c61fceb3d686a233
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2015-09-18 15:10:34 -0400

    WIP improve return values.

--- dataTree.py
+++ dataTree.py
@@ -55,7 +55,7 @@ class dataTree_PACSPull(dataTree):
     """
 
     def MRN_generate(self):
-        self.MRN = 10000000 + random.randint(0, 1000000)
+        self.MRN = 4000000 + random.randint(0, 1000000)
         return self.MRN
 
     def dbID_generate(self, a_upperLimit = 1000):
@@ -71,6 +71,26 @@ class dataTree_PACSPull(dataTree):
         with open('SeriesDescription.lst') as f:
             self.l_SeriesDescription = f.read().splitlines()
         return self.l_SeriesDescription
+
+    def SeriesInstanceUID_generate(self):
+        """
+        Build a "fake" SeriesInstanceUID
+        :return: a "fake" SeriesInstanceUID
+        """
+
+        return '%d.%d.%02d.%d.%04d.%d.%d.%02d.%05d.%010d' % \
+               (
+                   self.fake.random_int(0,          9),
+                   self.fake.random_int(0,          9),
+                   self.fake.random_int(0,         99),
+                   self.fake.random_int(0,          9),
+                   self.fake.random_int(0,       9999),
+                   self.fake.random_int(0,          9),
+                   self.fake.random_int(0,          9),
+                   self.fake.random_int(0,         99),
+                   self.fake.random_int(0,      99999),
+                   self.fake.random_int(0, 9999999999)
+               )
 
     def treeBuild(self):
         FS  = self.FS
@@ -88,6 +108,12 @@ class dataTree_PACSPull(dataTree):
                 for series in range(0, self.SeriesCount):
                     FS.mkcd('%s-%03d' % (random.choice(self.l_SeriesDescription).strip(),
                                          self.dbID_generate()))
+                    l_image = []
+                    for image in range(1, self.SeriesFilesCount + 1):
+                        l_image.append('%04d-%s.dcm' % \
+                            (image, self.SeriesInstanceUID_generate())
+                        )
+                    FS.touch('images', l_image)
                     FS.cd('../')
                 FS.cd('../')
             FS.cd('/')
@@ -106,12 +132,13 @@ class dataTree_PACSPull(dataTree):
         self.l_SeriesDescription    = self.SeriesDescriptions_generate()
 
         for key,val in kwargs.iteritems():
-            if key == 'MRNcount':           self.MRNCount           = val
-            if key == 'StudyCount':         self.StudyCount         = val
-            if key == 'SeriesCount':        self.SeriesCount        = val
-            if key == 'SeriesFilesCount':   self.SeriesFilesCount   = val
+            if key == 'MRNcount':           self.MRNCount           = int(val)
+            if key == 'StudyCount':         self.StudyCount         = int(val)
+            if key == 'SeriesCount':        self.SeriesCount        = int(val)
+            if key == 'SeriesFilesCount':   self.SeriesFilesCount   = int(val)
 
         self.treeBuild()
+        self.FS.tree_metaData_print(False)
 
 
 def synopsis(ab_shortOnly = False):
@@ -153,9 +180,17 @@ if __name__ == "__main__":
         action  =   'store',
         default =   "PACS_pull"
     )
+    parser.add_argument(
+        '-f', '--SeriesFilesCount',
+        help    =   "The number of simulated DICOM filenames to generate.",
+        dest    =   'SeriesFilesCount',
+        action  =   'store',
+        default =   '10'
+    )
 
     args        = parser.parse_args()
 
-    data        = dataTree_PACSPull()
+    data        = dataTree_PACSPull(SeriesFilesCount = args.SeriesFilesCount)
 
     print(data.FS)
+    print(json.dumps(dict(data.FS.snode_root)))
